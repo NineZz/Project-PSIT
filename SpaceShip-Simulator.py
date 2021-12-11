38dceaa036b5f846cfec387ffe369325c88ea882
REERen <88521278+REERen@users.noreply.github.com>
2021-12-11 21:02:18 +0700

    Update SpaceShip-Simulator.py

--- SpaceShip-Simulator.py
+++ SpaceShip-Simulator.py
@@ -83,6 +83,31 @@ class Enemy(Ship):
     def move(self, vel):
         self.y += vel
 
+#Laser
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+    
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+
+    def collision(self, odj):
+        return collide(odj, self)
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2, (offset_x, offset_y)) != None
+
 
 
 # ------------------------------------------------------------------------------
